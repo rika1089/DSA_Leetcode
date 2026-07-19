9ffb75af1bb6fae029869468775ddcc71cdaa39b
Rika <msreekarin@gmail.com>
2026-07-19 22:48:26 +0530

    Time: 3 ms (38.55%) | Memory: 19.4 MB (11.73%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,20 @@
+from collections import Counter
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        freq = Counter(s)
+        stack = []
+        in_stack = set()
+
+        for ch in s :
+            freq[ch] -= 1
+            if ch in in_stack :
+                continue 
+            
+            while stack and stack[-1] > ch and freq[stack[-1]] > 0 :
+                removed = stack.pop()
+                in_stack.remove(removed)
+
+            stack.append(ch)
+            in_stack.add(ch)
+
+        return "".join(stack)
