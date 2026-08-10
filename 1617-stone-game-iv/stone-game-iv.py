b1911e2e7c2a4ffb453bf0da15b13e68855f084c
Rika <msreekarin@gmail.com>
2026-08-10 23:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (97.88%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.py
+++ 1617-stone-game-iv/stone-game-iv.py
@@ -1,13 +1,25 @@
 class Solution:
+    MAX = 10**5
+    dp = [False] * (MAX + 1)
+
+    for i in range(MAX + 1):
+        if dp[i]: continue
+
+        for j in range(1, isqrt(MAX - i) + 1):
+            dp[i + j**2] = True
+
     def winnerSquareGame(self, n: int) -> bool:
-        @cache
-        def dfs(i) :
-            if i == 0 :
-                return False
+        return self.dp[n]
+# class Solution:
+#     def winnerSquareGame(self, n: int) -> bool:
+#         @cache
+#         def dfs(i) :
+#             if i == 0 :
+#                 return False
 
-            for j in range(1, isqrt(i)+1) :
-                if not dfs(i-j ** 2) :
-                    return True
-            return False
+#             for j in range(1, isqrt(i)+1) :
+#                 if not dfs(i-j ** 2) :
+#                     return True
+#             return False
 
-        return dfs(n)
+#         return dfs(n)
