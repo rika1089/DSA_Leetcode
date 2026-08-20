fe80cece6546a7747f539c0e90505948a6f2363a
Rika <msreekarin@gmail.com>
2026-08-20 18:57:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (62.03%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,14 +2,19 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         house_memo = {}
         def houserobber(i,nums) :
+            # Use of memoization
             if i in house_memo :
                 return house_memo[i]
+
+            # Base case well written
             if i >= len(nums) :
                 return 0
             
+            # Dynamic Programming
             rob = nums[i] + houserobber(i+2,nums)
             dontrob = houserobber(i+1,nums)
 
+            # Store the memoization
             house_memo[i] = max(rob , dontrob)
 
             return house_memo[i]
