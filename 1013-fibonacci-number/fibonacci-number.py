03ee760e1a2019a24423a62800010fd129ad75ee
Rika <msreekarin@gmail.com>
2026-08-20 18:31:37 +0530

    Time: 47 ms (62.64%) | Memory: 19.2 MB (57.79%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,24 +1,21 @@
 class Solution:
     
     def fib(self, n: int,fibonacci_Map = None) -> int:
-        if  fibonacci_Map is  None :
-            fibonacci_Map = {}
-
         
-        if n <= 1 :
+        fibo_map = {}
+
+        def fibonacci(n) :
+
+            if n <= 1 :
+                return n
             
-            return n
+            if n in fibo_map :
+                return fibo_map[n]
 
-        if n in fibonacci_Map :
-            return fibonacci_Map[n]
+            a = fibonacci(n-1)
+            b = fibonacci(n-2)
 
+            fibo_map[n] = a + b 
+            return fibo_map[n]
 
-        a = self.fib(n-1,fibonacci_Map )
-        b = self.fib(n-2,fibonacci_Map )
-
-        fibonacci_Map[n] = a+b
-
-        return fibonacci_Map[n]
-
-
-        
+        return fibonacci(n)
