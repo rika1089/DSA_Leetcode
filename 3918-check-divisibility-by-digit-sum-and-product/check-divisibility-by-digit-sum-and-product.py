daad81aa5452bfc423736294a21cf6a7495029fb
Rika <msreekarin@gmail.com>
2026-08-22 23:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (58.35%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
@@ -1,9 +1,11 @@
 class Solution:
     def checkDivisibility(self, n: int) -> bool:
-        SUM = 0
-        PROD = 1
-        for digit in list(str(n)) :
-            SUM += int(digit)
-            PROD *= int(digit)
+        # SUM = 0
+        # PROD = 1
+        # for digit in list(str(n)) :
+        #     SUM += int(digit)
+        #     PROD *= int(digit)
         
-        return n % ( SUM + PROD ) == 0 
+        # return n % ( SUM + PROD ) == 0 
+
+        return  n % (sum(ln:= [(n//p)%10 for i in range(7) if (p:=10**i) <=n]) + prod(ln)) == 0
