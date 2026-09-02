27bcd34eda70154dc4fed3274b38af9860d32c0c
Rika <msreekarin@gmail.com>
2026-09-02 23:39:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (84.34%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -1,3 +1,11 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
+
+        # Equal parity produce and "even" number as diff
+        # DIff parity produce and "odd" number as diff
+        #  E - E = E            O - O = E
+        #  E - O = O            O - E = O
+
+        # so however only even or odd are produced which satisfy the given question
+        # Therefore, we simply return true.
         return True
