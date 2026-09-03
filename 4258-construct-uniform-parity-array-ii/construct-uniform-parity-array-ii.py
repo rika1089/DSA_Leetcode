eaddf5dc548395838893e966b3a60d5a896259bc
Rika <msreekarin@gmail.com>
2026-09-04 00:15:39 +0530

    Time: 99 ms (25.00%) | Memory: 35.2 MB (73.17%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,42 +1,43 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        # if not nums1 :
-        #     return True
-
-        # nums = sorted(nums1)
-
-        # minele_parity = nums[0] & 1 
-
-        # # let seen[0] = even seen[1] = odd
-        # seen = [False, False]
-
-        # # lets assume smallest ele as seen
-        # seen[minele_parity] = True
-
-        # for x in nums[1:] :
-        #     p = x & 1 
-        #     if p == minele_parity :
-        #         seen[p] = True
-        #         continue
-            
-        #     # need a small with parity = p XOR minele_parity
-        #     required = p ^ minele_parity
-
-        #     if not seen[required] :
-        #         return False
-            
-
-        #     seen[p] = True
-
-        # return True
-
-        if min(nums1) % 2:
+        if not nums1 :
             return True
 
-        f = True
-        for num in nums1:
-            if num % 2:
-                f = False
-                break
+        nums = sorted(nums1)
+
+        minele_parity = nums[0] & 1 
+
+        # let seen[0] = even seen[1] = odd
+        seen = [False, False]
+
+        # lets assume smallest ele as seen
+        seen[minele_parity] = True
+
+        for x in nums[1:] :
+            p = x & 1 
+            if p == minele_parity :
+                seen[p] = True
+                continue
+            
+            # need a small with parity = p XOR minele_parity
+            required = p ^ minele_parity
+
+            if not seen[required] :
+                return False
+            
+
+            seen[p] = True
+
+        return True
+
+        # # if smallest is odd it always satisfies
+        # if min(nums1) % 2: # since it return 1
+        #     return True
+
+        # f = True
+        # for num in nums1:
+        #     if num % 2:
+        #         f = False
+        #         break
         
-        return f
+        # return f
