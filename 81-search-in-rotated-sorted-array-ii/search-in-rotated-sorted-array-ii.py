1c634df778d66ab4faff0609d83a66ded3810740
Rika <msreekarin@gmail.com>
2026-09-05 23:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (97.67%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,25 +1,45 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        low , high = 0,len(nums)-1
-        while low <= high : 
-            mid = low + (high-low)//2
-            if nums[mid] == target :
-                return True
-            if nums[low] == nums[mid] == nums[high] :
-                low = low + 1
-                high = high - 1
-                continue
-            if nums[low] <= nums[mid] :
-                if nums[low] <= target and target <= nums[mid] :
-                    high = mid - 1
-                else :
-                    low = mid + 1
-            else :
-                if nums[mid] <= target and target <= nums[high] :
-                    low = mid + 1
-                else :
-                    high = mid - 1
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
 
-        return False
+        # TC : O(n)
+        # for i,val in enumerate(nums) :
+        #     if val == target :
+        #         return i
 
+        # return -1
         
+        # Terms : 
+        # 1 . Fixed Length (limited search space)
+        # 2 . sorted
+
+        l,h = 0 ,len(nums)-1
+
+        while l <= h :
+            mid = l + (h-l)//2
+
+            if nums[mid] == target :
+                return True # mid
+                
+            if nums[l] == nums[mid] and nums[mid] == nums[h] :
+                l += 1
+                h -= 1
+                continue
+
+            if nums[l] <= nums[mid] :
+                if nums[l] <= target and target <= nums[mid] :
+                    h = mid - 1
+                else :
+                    l = mid + 1
+            
+            else : # if nums[mid] < nums[h] :
+                if nums[mid] <= target and target <= nums[h] :
+                    l = mid + 1
+                else :
+                    h = mid - 1
+
+        return False #-1
