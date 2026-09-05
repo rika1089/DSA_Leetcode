2ed6096de9b2191befa934f64de7d8f2d60f7554
Rika <msreekarin@gmail.com>
2026-09-05 23:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (81.19%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,30 +1,14 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        low = 0
-        high = n-1
-        while low <= high : 
-            mid = low + (high - low) // 2
-            if nums[mid] == target :
-                return mid
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
 
-            if nums[low] <= nums[mid] :
-                if nums[low] <= target and target <= nums[mid] :
-                    high = mid - 1
-                else :
-                    low = mid + 1
-                
-            else :
-                if nums[mid] <= target and target <= nums[high] :
-                    low  = mid + 1
-                else :
-                    high = mid - 1
+        for i,val in enumerate(nums) :
+            if val == target :
+                return i
 
         return -1
-            
-            
-
-
-
-
         
