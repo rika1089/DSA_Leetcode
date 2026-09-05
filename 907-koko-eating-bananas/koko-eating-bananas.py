a8c21bb0f12c09fd18a31524b5a3b7be47c739df
Rika <msreekarin@gmail.com>
2026-09-06 00:29:58 +0530

    Time: 182 ms (37.40%) | Memory: 13 MB (99.01%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,19 +1,32 @@
-class Solution:
-    def eatbanana(self,piles:List[int],k:int)->int :
-        hrs = 0
-        for pile in piles :
-            hrs += math.ceil(pile/k)
-        return hrs
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        low = 1
-        high = max(piles)
-        ans = -1
+class Solution(object):
+    def totalhrs(self,piles,k) :
+        total_hrs = 0
+        for i in range(len(piles)) :
+            total_hrs += (piles[i]+k-1) // k
+        return total_hrs
 
+
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+
+        low = 1
+        high = max(piles) 
+        ans = high
         while low <= high :
-            mid = low + ( high - low ) // 2
-            if self.eatbanana(piles,mid) <= h :
+            mid = low + (high-low) // 2
+
+            if self.totalhrs(piles,mid) <= h :
                 ans = mid
                 high = mid - 1
+            
             else :
                 low = mid + 1
+        
         return ans
+
+
+        
