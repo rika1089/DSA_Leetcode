6b60bcbd3f966e05c2fe528aedb5a1d95311e5d3
Rika <msreekarin@gmail.com>
2026-09-06 22:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (91.15%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,21 +1,37 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        i = 0
-        stack = []
+        # i = 0
+        # stack = []
 
-        for i in range(len(s)) :
-            if s[i] =='(' or s[i] =='{' or s[i] == '[' :
-                stack.append(s[i])
+        # for i in range(len(s)) :
+        #     if s[i] =='(' or s[i] =='{' or s[i] == '[' :
+        #         stack.append(s[i])
             
-            else :
-                if not stack :
-                    return False
+        #     else :
+        #         if not stack :
+        #             return False
                 
-                top  = stack.pop()
-                if s[i] == ')' and top != '(' :
-                    return False
-                if s[i] == '}' and top != '{' :
-                    return False
-                if s[i] == ']' and top != '[' :
-                    return False
+        #         top  = stack.pop()
+        #         if s[i] == ')' and top != '(' :
+        #             return False
+        #         if s[i] == '}' and top != '{' :
+        #             return False
+        #         if s[i] == ']' and top != '[' :
+        #             return False
+        # return len(stack) == 0
+
+        Map = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{',
+        }
+        stack = []
+        for i in range(len(s)) :
+            if s[i] in Map :
+                    if not stack or  Map[s[i]] != stack.pop() :
+                        return False
+            else :
+                stack.append(s[i])
+        
         return len(stack) == 0
+
