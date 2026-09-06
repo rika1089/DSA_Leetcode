f0c77aa04da6b38c2143f92f27ac3c24a4ecfa83
Rika <msreekarin@gmail.com>
2026-09-06 22:01:17 +0530

    Time: 39 ms (27.75%) | Memory: 21.9 MB (56.39%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,18 +1,25 @@
 from collections import defaultdict
+from typing import List
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        n = len(nums)
         Hash = defaultdict(int)
-        Hash[0] = 1
+        Hash[0] = 1 # Store 0 PrefixSum also in the Hash (whihc is very very significant)
         cnt = 0
+
         prefixSum = 0
-        for i in range(n) :
+
+        for i in range(len(nums)) :
             prefixSum += nums[i]
 
             remove = prefixSum - k
+
             if remove in Hash :
                 cnt += Hash[remove]
-            
+
             Hash[prefixSum] += 1
-        
+
         return cnt
+
+
+        
