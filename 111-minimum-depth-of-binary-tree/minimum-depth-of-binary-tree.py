ffaf91dd331c10989763e6902cbbcf9ec4efd6b4
Rika <msreekarin@gmail.com>
2026-09-07 22:46:11 +0530

    Time: 1 ms (82.10%) | Memory: 44.8 MB (63.69%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,24 +6,40 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        # DFS 
-        def dfs(node) :
-            # If the subtree is empty i.e. root is NULL, return depth as 0
-            if not node :
-                return 0
+        # # DFS 
+        # def dfs(node) :
+        #     # If the subtree is empty i.e. root is NULL, return depth as 0
+        #     if not node :
+        #         return 0
             
-            # Initialize the depth of two subtrees.
-            if not node.left and not node.right :
-                return 1
+        #     # Initialize the depth of two subtrees.
+        #     if not node.left and not node.right :
+        #         return 1
 
-            # If the left subtree is empty, return the depth of right subtree after adding 1 to it
-            if not node.left :
-                return 1 + dfs(node.right)
+        #     # If the left subtree is empty, return the depth of right subtree after adding 1 to it
+        #     if not node.left :
+        #         return 1 + dfs(node.right)
 
-            # If right subtree is empty , return the depth of left subtree after adding 1 to it
-            if not node.right :
-                return 1 + dfs(node.left)
+        #     # If right subtree is empty , return the depth of left subtree after adding 1 to it
+        #     if not node.right :
+        #         return 1 + dfs(node.left)
 
-            return 1 + min(dfs(node.left),dfs(node.right))
+        #     return 1 + min(dfs(node.left),dfs(node.right))
         
-        return dfs(root)
+        # return dfs(root)
+
+        # BFS
+        if root is None: return 0
+        depth = 1
+        q = [root]
+        while q:
+            sz = len(q)
+            for i in range(sz):
+                cur = q.pop(0)
+                if (cur.left is None) and (cur.right is None): return depth
+                if cur.left: q.append(cur.left)
+                if cur.right: q.append(cur.right)
+            
+            depth += 1
+        
+        return depth
