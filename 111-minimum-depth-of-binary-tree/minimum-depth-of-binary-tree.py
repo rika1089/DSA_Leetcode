72cdc9c18e72aceb67789e893f42e13c6dc0679d
Rika <msreekarin@gmail.com>
2026-09-07 22:45:00 +0530

    Time: 43 ms (34.24%) | Memory: 45 MB (53.32%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        # DFS 
+        def dfs(node) :
+            # If the subtree is empty i.e. root is NULL, return depth as 0
+            if not node :
+                return 0
+            
+            # Initialize the depth of two subtrees.
+            if not node.left and not node.right :
+                return 1
+
+            # If the left subtree is empty, return the depth of right subtree after adding 1 to it
+            if not node.left :
+                return 1 + dfs(node.right)
+
+            # If right subtree is empty , return the depth of left subtree after adding 1 to it
+            if not node.right :
+                return 1 + dfs(node.left)
+
+            return 1 + min(dfs(node.left),dfs(node.right))
+        
+        return dfs(root)
