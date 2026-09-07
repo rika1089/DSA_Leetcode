2ba410863b4d2a9d88b4dc799c2614206fb1e6c1
Rika <msreekarin@gmail.com>
2026-09-07 21:52:58 +0530

    Time: 79 ms (96.10%) | Memory: 28.3 MB (62.35%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -7,10 +7,11 @@ class Solution:
             return [0]
         
         #stack = [temperatures[0]]
-        stack = list()
+        stack = list() # Stores indices not values
         res = [0] * n
 
         for i in range(n) :
+            # Resolve all indices whose temp is less than curr
             while stack and temperatures[i] > temperatures[stack[-1]]:
                 idx = stack.pop()
                 res[idx] = i - idx
