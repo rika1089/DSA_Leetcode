4385ea9d00b1efee90deec3fd5bc2811603591fc
Rika <msreekarin@gmail.com>
2026-09-07 22:08:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (29.98%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -6,16 +6,35 @@ class Solution:
         if n1 > n2 :
             return None
         
-        res = [-1]*n1
+        # res = [-1]*n1
 
-        for i in range(n1) :
-            pos = nums2.index(nums1[i])
-            j = pos + 1
-            if nums1[i] in nums2 :
-                while j < n2 :
-                    if nums1[i] < nums2[j] :
-                        res[i] = nums2[j]
-                        break
-                    j += 1
+        # for i in range(n1) :
+        #     pos = nums2.index(nums1[i])
+        #     j = pos + 1
+        #     if nums1[i] in nums2 :
+        #         while j < n2 :
+        #             if nums1[i] < nums2[j] :
+        #                 res[i] = nums2[j]
+        #                 break
+        #             j += 1
+        # return res
+        hmap = {}
+        stack = []
+
+        for num in nums2 :
+            while stack and num > stack[-1] :
+                popped = stack.pop()
+                hmap[popped] = num
+            stack.append(num)
+
+        res = []
+        for num in nums1 :
+            if num in hmap :
+                res.append(hmap[num])
+            else :
+                res.append(-1)
         return res
+
+
+
 
