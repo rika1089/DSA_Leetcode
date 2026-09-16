00d60b4e02dd3dc4099f715b67b269d132405b8e
Rika <msreekarin@gmail.com>
2026-09-17 00:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (68.10%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,15 +1,15 @@
 MOD = 10 ** 9 + 7
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        # return math.comb(n+k-1, 2*k) % MOD
-        dp = [1] * n
-        prefix_sums = [0] * (n + 1)
-        for j in range(n):
-            prefix_sums[j + 1] = (prefix_sums[j] + dp[j]) % MOD
-        for _ in range(k):
-            dp[0] = 0
-            for j in range(1, n):
-                dp[j] = (dp[j - 1] + prefix_sums[j]) % MOD
-            for j in range(n):
-                prefix_sums[j + 1] = (prefix_sums[j] + dp[j]) % MOD
-        return dp[n - 1]
+        return math.comb(n+k-1, 2*k) % MOD
+        # dp = [1] * n
+        # prefix_sums = [0] * (n + 1)
+        # for j in range(n):
+        #     prefix_sums[j + 1] = (prefix_sums[j] + dp[j]) % MOD
+        # for _ in range(k):
+        #     dp[0] = 0
+        #     for j in range(1, n):
+        #         dp[j] = (dp[j - 1] + prefix_sums[j]) % MOD
+        #     for j in range(n):
+        #         prefix_sums[j + 1] = (prefix_sums[j] + dp[j]) % MOD
+        # return dp[n - 1]
