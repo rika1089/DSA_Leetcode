b9df08547f866600bbb07364e46d17141e72c1b6
Rika <msreekarin@gmail.com>
2026-09-21 23:26:56 +0530

    Time: 3775 ms (5.26%) | Memory: 411.2 MB (5.26%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -0,0 +1,37 @@
+class Solution:
+    def resultArray(self, nums, k):
+        n = len(nums)
+        nums = [x % k for x in nums]
+        res = [0] * k
+
+        for req in range(k):
+            dp = {}
+
+            def solve(i, prevProd):
+                if i >= n:
+                    return 0
+                if (i, prevProd) in dp:
+                    return dp[(i, prevProd)]
+
+                skip = 0
+                take = 0
+
+                # Skip current element (only if no subarray started yet)
+                if prevProd == k:
+                    skip = solve(i + 1, k)
+
+                # Take current element
+                if prevProd == k:
+                    curProd = nums[i]
+                else:
+                    curProd = (prevProd * nums[i]) % k
+
+                take += 1 if curProd == req else 0
+                take += solve(i + 1, curProd)
+
+                dp[(i, prevProd)] = take + skip
+                return dp[(i, prevProd)]
+
+            res[req] = solve(0, k)
+
+        return res
